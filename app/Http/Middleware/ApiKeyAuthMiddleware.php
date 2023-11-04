135e18dfaf3b8fe1dff2e42510bf51b4be0eeaae
xichanggumeng <1228396112@qq.com>
2023-11-04 11:26:18 +0800

    feat: api鉴权

---
+++ app/Http/Middleware/ApiKeyAuthMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiKeyAuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $apiKey = config('app.api_key'); // 从配置文件获取 API_KEY
+
+        $requestKey = $request->input('key'); // 获取请求中的 key 参数
+
+        if ($requestKey !== $apiKey) {
+            return response('Unauthorized', 401); // 鉴权失败，返回 401 Unauthorized 响应
+        }
+
+        return $next($request);
+    }
+}
